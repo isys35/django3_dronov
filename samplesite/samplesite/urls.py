88ed72a3e0c7fbc26b3313e34f805c13acad9e96
isys35 <isys35@mail.ru>
2020-12-08 11:27:30 +0300

    fix

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('bboard.urls')),
     path('bboard/', include('bboard.urls')),
     path('admin/', admin.site.urls),
 ]
