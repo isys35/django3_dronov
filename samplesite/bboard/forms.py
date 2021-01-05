cf462e80c78bc3e08df856b332a9d534ebeb19a9
isys35 <isys35@mail.ru>
2021-01-05 16:48:04 +0300

    Глава 13. В процессе

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,9 +1,34 @@
 from django.forms import ModelForm
+from django.forms.widgets import Select
+from django.forms import modelform_factory, DecimalField
 
 from .models import Bb
+
+"""
+    Быстрое объявление формы, связанной с моделью
+    Листинг 13.2
+"""
 
 
 class BbForm(ModelForm):
     class Meta:
         model = Bb
         fields = ('title', 'content', 'price', 'rubric')
+        labels = {'title': 'Название товара'}
+        help_texts = {'rubric': 'Не забудьте задать рубрику'}
+        field_classes = {'price': DecimalField}
+        widgets = {'rubric': Select(attrs={'size': 8})}
+
+
+"""
+    Использование фабрики классов modelform_factory()
+    Листинг 13.1
+"""
+
+#
+# BbForm = modelform_factory(Bb,
+#                            fields=('title', 'content', 'price', 'rubric'),
+#                            labels={'title': 'Название товара'},
+#                            help_texts={'rubric': 'Не забудьте выбрать рубрику!'},
+#                            field_classes={'price': DecimalField},
+#                            widgets={'rubric': Select(attrs={'size':8})})
