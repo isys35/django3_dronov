b14e3fe9478d8fee1ca91808e75296a0140f3173
isys35 <isys35@mail.ru>
2021-02-01 22:56:29 +0300

    Глава 13. В процессе

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -32,3 +32,62 @@ class BbForm(ModelForm):
 #                            help_texts={'rubric': 'Не забудьте выбрать рубрику!'},
 #                            field_classes={'price': DecimalField},
 #                            widgets={'rubric': Select(attrs={'size':8})})
+
+
+"""
+    Полное объявление формы
+    Листинг 13.3
+"""
+# from django import forms
+# from .models import Bb, Rubric
+#
+#
+# class BbForm(forms.ModelForm):
+#     title = forms.CharField(label='Название товара')
+#     content = forms.CharField(label='Описание', widget=forms.widgets.Textarea())
+#     price = forms.DecimalField(label='Цена', decimal_places=2)
+#     rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(),
+#                                     label='Рубрика',
+#                                     help_text='Не забудьте задать рубрику!',
+#                                     widget=forms.widgets.Select(attrs={'size': 8}))
+#
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'rubric')
+
+"""
+    Полное объявление отдельных полей формы
+    Листинг 13.4
+"""
+# from django import forms
+# from .models import Bb, Rubric
+#
+#
+# class BbForm(forms.ModelForm):
+#     price = forms.DecimalField(label='Цена', decimal_places=2)
+#     rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(),
+#                                     label='Рубрика', help_text='Не забудьте задать рубрику!',
+#                                     widget=forms.widgets.Select(attrs={'size': 8}))
+#
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'rubric')
+#         labels = {'title': 'Название товара'}
+
+
+"""
+    Объявление в форме полей, не существующих в связанной модели
+    Листинг 13.5
+"""
+
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterUserForm(forms.ModelForm):
+    password1 = forms.CharField(label='Пароль')
+    password2 = forms.CharField(label='Пароль (повторнорно)')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
