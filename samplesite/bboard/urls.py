658ac1b14c354cb7a229273cd90d0d8d3b7afa8b
isys35 <isys35@mail.ru>
2021-02-26 12:08:36 +0300

    Глава 15. В процессе

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView, \
+    PasswordChangeView, PasswordChangeDoneView, PasswordResetView, \
+    PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from .views import *
 
 app_name = 'bboard'
@@ -9,13 +11,33 @@ urlpatterns = [
     path('add/', BbCreateView.as_view(), name='add'),
     path('edit/<int:pk>/', BbEditView.as_view(), name='edit'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    #path('', BbIndexView.as_view(), name='index')
+    # path('', BbIndexView.as_view(), name='index')
     path('', index, name='index'),
     path('rubrics', rubrics, name='rubrics'),
-    path('accounts/login/', LoginView.as_view(), name='login')
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(next_page='bboard:index'), name='logout'),
+    path('accounts/password_change/', PasswordChangeView.as_view(template_name='registration/change_password.html'),
+         name='password_change'),
+    path('accounts/password_change/done/',
+         PasswordChangeDoneView.as_view(template_name='registration/password_changed.html'),
+         name='password_change_done'),
+    path('accounts/password_reset/',
+         PasswordResetView.as_view(template_name='registration/reset_password.html',
+                                   subject_template_name='registration/reset_subjects.txt',
+                                   email_template_name='registration/reset_email.txt'
+                                   ),
+         name='password_reset'),
+    path('accounts/password_reset/done/',
+         PasswordResetDoneView.as_view(template_name='registration/email_sent.html'),
+         name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='registration/confirm_password.html'),
+         name='password_reset_confirm'),
+    path('accounts/reset/done',
+         PasswordResetCompleteView.as_view(template_name='registration/password_confirmed.html'),
+         name='password_reset_complete'),
     # path('detail/<int:year>/<int:month>/<int:day>/<int:pk>/', BbDetailView.as_view(), name='delete'),
 ]
-
 
 """
     Указание шаблонных путей в виде регулярных выражений
