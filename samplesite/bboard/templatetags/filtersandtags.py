17cf91c27d0752caee088d4bc85de745927a3a3f
isys35 <isys35@mail.ru>
2021-03-23 15:33:13 +0300

    Глава 19.Завершено

---
+++ samplesite/bboard/templatetags/filtersandtags.py
@@ -0,0 +1,40 @@
+"""
+    Листинг 19.1 Пример создания фильтра
+"""
+
+# from django import template
+#
+# register = template.Library()
+#
+#
+# def currency(value, name='руб. '):
+#     return '%1.2f %s' % (value, name)
+#
+#
+# register.filter('currency', currency)
+
+"""
+    Листинг 19.2 Пример объявления тега, выводящего элементарное значение
+"""
+
+# from django import template
+#
+# register = template.Library()
+#
+#
+# @register.simple_tag
+# def lst(sep, *args):
+#     return '%s (итого %s)' % (sep.join(args), len(args))
+
+"""
+    Листинг 19.3 Пример шаблонного тега
+"""
+
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('tags/ulist.html')
+def ulist(*args):
+    return {'items': args}
